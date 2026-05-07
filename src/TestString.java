213ef50c5ef228d4d481342ae0e0f1ca22ef2423
sumanghosh02 <work.sumanghosh1@gmail.com>
2026-05-07 21:26:17 +0530

    Added Java practice files

---
+++ src/TestString.java
@@ -0,0 +1,7 @@
+public class TestString {
+    public static void main(String[ ] args) {
+        String name = "Suman";
+        String address = new String("India");
+        System.out.println(name);
+    }
+}
---
+++ src/TestStudent.java
@@ -0,0 +1,10 @@
+public class TestStudent {
+    public static void main(String[ ] args) {
+        Student st = new Student();
+        st.name = "Suman";
+        st.address = "Kolkata";
+        st.roll = 36;
+        st.standard = 1;
+        System.out.println(st.address);
+    }
+}
